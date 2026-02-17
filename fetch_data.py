070f4bbfb9c21725b254a54001712b6934363d6c
Chandra-Madhubani <chandra20052020@gmail.com>
2026-02-17 16:54:17 +0530

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 import pandas as pd
 
 
-API_KEY = "jfircrke4rck"   
+API_KEY = "${{ secrets.EBIRD_API_KEY }}"   
 REGION_CODE = "IN-MH"    
 BACK_DAYS = 30        
 BASE_URL = "https://api.ebird.org/v2/data/obs"
@@ -27,8 +27,10 @@ if response.status_code == 200:
     # Print a few examples
     #for i, obs in enumerate(data[:10]):
         #print(f"{i+1}. Species: {obs.get('comName')} | Date: {obs.get('obsDt')}")
-    #else:
-        #print("Error:", response.status_code, response.text)
+    else:
+        print("Error:", response.status_code, response.text)
 
-
+month_year = today.strftime("%B_%Y")
 data = pd.DataFrame(data)
+df.to_csv(f"months/{month_year}", index=False)
+data.to_csv(f"months/{month_year}/{month_year}.csv")
