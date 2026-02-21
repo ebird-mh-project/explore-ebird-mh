a31caca2bd6cd1f4adf117e96c0f5944c2831533
Chandra-Madhubani <chandra20052020@gmail.com>
2026-02-22 00:55:05 +0530

    Create update2025_seasonmonthly.py

---
+++ update2025_seasonmonthly.py
@@ -0,0 +1,14 @@
+'''
+21/02/2026
+This will fetch data + generate monthly map & summary (generate_map.py & generate_summary.py) + generate seasonal map & summary (generate_summary_seasonal.py)
+for all of 2025 + for January 2026......... then, an 'initial.yml' will be ran , in order to run THIS just once + to see if there's any issues which arise
+(like with labelling, for example will it be labelled 'February' if it's executed on 1 February? It takes the 'last 30 days', too... maybe make that a
+regular interval of 30 days instead of once every month? And also, it only lasts till the end of 2026. Change that in the YAML file.)
+But yeah. This will be the 'test run'. Populate with the data based on the new process. Old version will be off to the side.
+
+After this: update About page very briefly
+
+^^^ This stuff will pretty much finish up this dashboard for the most part. Tweaks needed, but it'll be overall presentable.
+
+then get to the SDM project, with this dashboard as the foundation for the presentation (but more DYNAMIC; have more interesting UI elements to select new variables)
+'''
